de8026e520a693c85aa29f81965132bfb83f716a
Edward Kim <itgma1231@gmail.com>
2023-10-29 14:46:16 -0400

    typeDefs.js inital start

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,3 +1,31 @@
-const typeDefs = {}
+const typeDefs = `
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        bookCount: Int
+        savedBooks: [Book]!
+    }
+
+    type Book {
+        _id: ID
+        authors: [String]
+        description: String
+        bookId: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+    }
+`
 
 module.exports = typeDefs;
