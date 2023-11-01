a821004139b5ea2be6ce529847b2554425108682
Edward Kim <itgma1231@gmail.com>
2023-11-01 15:41:57 -0400

    ADD_USER/SAVE_BOOK/REMOVE_BOOK mutations.js completed

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -11,3 +11,53 @@ export const LOGIN_USER = gql`
         }
     }
 `
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($bookSaved: bookInput) {
+        saveBook(bookSaved: $bookSaved) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`
