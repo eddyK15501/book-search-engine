5cf5f490072d48ed2bfb81c106c4e0dd06f5bcab
Edward Kim <itgma1231@gmail.com>
2023-11-01 17:02:23 -0400

    vite.config.js proxy endpoint fix; server.js fix

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -11,27 +11,18 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(routes);
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(routes);
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
-}
-
 const startApolloServer = async () => {
   await server.start();
+  
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
   app.use(
     "/graphql",
@@ -39,6 +30,14 @@ const startApolloServer = async () => {
       context: authMiddleware,
     })
   );
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    });
+  }
 
   db.once("open", () => {
     app.listen(PORT, () => {
